74cd44ca14154f1827d90400a4bd5465190d7339
fredowisk <zcrazyhero@gmail.com>
2023-06-16 20:57:50 -0300

    feat: change language on render

--- src/contexts/TranslateContext.tsx
+++ src/contexts/TranslateContext.tsx
@@ -1,6 +1,5 @@
 import React, { ReactNode, createContext, useState } from "react";
-import { useTranslation } from "next-i18next";
-import { TFunction } from "next-i18next";
+import { useTranslation, TFunction } from "next-i18next";
 
 interface TranslateContextValues {
   language: { abbreviation: string; url: string; flag: string };
@@ -20,7 +19,10 @@ export const TranslationProvider = ({ children }: TranslationProviderProps) => {
     url: "https://s.w.org/images/core/emoji/14.0.0/svg/1f1fa-1f1f8.svg",
     flag: ":us:",
   });
-  const { t: translate } = useTranslation("common");
+  const {
+    t: translate,
+    i18n: { changeLanguage },
+  } = useTranslation("common");
 
   const toggleLanguage = () => {
     setLanguage(
@@ -36,6 +38,8 @@ export const TranslationProvider = ({ children }: TranslationProviderProps) => {
             flag: "🇺🇸",
           }
     );
+
+    changeLanguage(language.abbreviation);
   };
 
   return (
