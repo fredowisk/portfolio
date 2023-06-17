c99dbc91edb078fc175d92ba8205d50d12513102
fredowisk <zcrazyhero@gmail.com>
2023-06-17 12:08:13 -0300

    update image links

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -7,7 +7,7 @@ export function Content() {
     <div className={styles.container}>
       <div className={styles.imageContainer}>
         <img
-          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.lorenzoribecca.co.uk%2Fwp-content%2Fuploads%2F2020%2F06%2FNatours-1536x693.png&f=1&nofb=1&ipt=017930fd2d642ab5773072ab8e42ab401e805c9db4eb78faed46e4207b70f1dc&ipo=images"
+          src="https://github.com/fredowisk/Natours/assets/53921083/5b875b83-ddb6-4dd0-81cd-49d95c2d9ac7"
           alt="Natours app"
         />
         <div className={styles.imageDescription}>
@@ -24,7 +24,7 @@ export function Content() {
       </div>
       <div className={styles.imageContainer}>
         <img
-          src="https://github.com/fredowisk/forkify/blob/main/src/img/screenshot.jpg?raw=true"
+          src="https://raw.githubusercontent.com/fredowisk/forkify/main/src/img/screenshot.jpg"
           alt="Forkify app"
         />
         <div className={styles.imageDescription}>
@@ -41,7 +41,7 @@ export function Content() {
       </div>
       <div className={styles.imageContainer}>
         <img
-          src="https://user-images.githubusercontent.com/53921083/230407174-d189e6ef-9575-4d61-a1c9-8e6ac973934f.png"
+          src="https://user-images.githubusercontent.com/53921083/230407191-d318997d-7fcc-4884-93b5-681291baadb9.png"
           alt="dtMoney app"
         />
         <div className={styles.imageDescription}>
@@ -75,7 +75,7 @@ export function Content() {
       </div>
       <div className={styles.imageContainer}>
         <img
-          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcamo.githubusercontent.com%2Fafaa4f032c9f2caa0bdb99ec011a87d1ce3a10986e21d978a975d7495a7f4130%2F68747470733a2f2f692e696d6775722e636f6d2f7941794b6e35762e706e67&f=1&nofb=1&ipt=cd816eb84a0043efff574c8ece8ceb031876f2743cf3fadc24c48b19a8fd200b&ipo=images"
+          src="https://github.com/fredowisk/dashgo/assets/53921083/3690d4de-48b3-4550-947f-78fc831f8987"
           alt="dashgo app"
         />
         <div className={styles.imageDescription}>
@@ -92,7 +92,7 @@ export function Content() {
       </div>
       <div className={styles.imageContainer}>
         <img
-          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fres.cloudinary.com%2Flukemorales%2Fimage%2Fupload%2Fv1563042321%2Freadme_logos%2Frocketshoes_readme_swvhr9.png&f=1&nofb=1&ipt=ac3c4c54f837f21e79f323b7fb2ae68ea9b8f3771e3e6e0bf6cc459bd7acb4d5&ipo=images"
+          src="https://github.com/fredowisk/RocketShoes/assets/53921083/de9c989f-cd17-4571-94c0-23039f972457"
           alt="Rocketshoes app"
         />
         <div className={styles.imageDescription}>
